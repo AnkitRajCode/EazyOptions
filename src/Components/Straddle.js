e415d1adbb7c730fa2d6f01d038f927a16d7874f
Yashvarshney02 <varshneyyash60@gmail.com>
2021-10-16 01:07:18 +0530

    Added Options Chart Functionality

--- src/Components/Straddle.js
+++ src/Components/Straddle.js
@@ -1,126 +1,185 @@
-import React, { Component } from "react";
+import React from "react";
+import { useState } from "react";
 import Chart from "react-apexcharts";
 import Sidebar from "./Sidebar";
 import '../Css/Straddle.css';
 
-class Straddle extends Component {
+export default function Straddle() {
+  const [shouldDisplay, setShouldDisplay] = useState("none")
+  const [instrument, setInstrument] = useState("")
+  const [strike, setStrike] = useState();
+  const [date, setDate] = useState();
+  const [chartOptions, setChartOptions] = useState({});
+  const [chartSeries, setChartSeries] = useState([]);
   
-  constructor(props) {
+
+  function fetchResults(e) {
+    e.preventDefault();
+    let X = [];
+    let Y = [];
     const data = {
-      'name': "NIFTY BANK",
-      'strike': 38000,
-      'date': '2021-10-11'
+      'name': instrument,
+      'strike': strike,
+      'date': date
     }
     const Data = JSON.stringify(data);
     const url = 'https://kpiro.com/charts';
-    const x_axis=[];
-    const y_axis=[];
     const requestOptions = {
       method: "POST",
       headers: { 'Content-Type': 'application/json' },
       body: Data
     };
-    function fetchResults() {
-
     fetch(url, requestOptions).then(
       (response) => {
         response.json().then(
           (data) => {
             let x = data["data"]
             for (var e in x) {
-              x_axis.push(x[e][0])
-              y_axis.push(x[e][1])
+              X.push(x[e][0])
+              Y.push(x[e][1])
             }
+            var line = {
+              chart: {
+                id: "basic-bar",
+                height: '100%'
+              },
+              xaxis: {
+                categories: X,
+                tickPlacement: 'between',
+                tickAmount: 10,
+                labels: {
+                  show: true,
+                  rotate: -45,
+                  rotateAlways: false,
+                  hideOverlappingLabels: true,
+                  showDuplicates: false,
+                  trim: true,
+                  minHeight: undefined,
+                  maxHeight: 400,
+                  style: {
+                    colors: [],
+                    fontSize: '12px',
+                    fontFamily: 'Helvetica, Arial, sans-serif',
+                    fontWeight: 400,
+                    cssClass: 'apexcharts-xaxis-label',
+                  },
+                  offsetX: 0,
+                  offsetY: 0,
+                  format: undefined,
+                  formatter: undefined,
+                  datetimeUTC: true,
+                  datetimeFormatter: {
+                    year: 'yyyy',
+                    month: "MMM 'yy",
+                    day: 'dd MMM',
+                    hour: 'HH:mm',
+                  },
+                }
+              }
+            }
+
+            var series = [
+              {
+                name: "Combined Premium",
+                data: Y
+              }
+            ]
+
+            setChartSeries(series);
+
+            setChartOptions(line);
+
+            setShouldDisplay("");
+
           }
         )
       }
+
     )
 
+
+
+
   }
-  fetchResults();
-    super(props);
 
-    this.state = {
-      options: {
-        chart: {
-          id: "basic-bar"
-        },
-        xaxis: {
-          categories: x_axis
-        }
-      },
-      series: [
-        {
-          name: "series-1",
-          data: y_axis
-        }
-      ]
-    };
-}
+  function handleSubmit(e) {
+    fetchResults(e);
+  }
 
-render() {
-    return (
-        <div className="straddle">
-            <Sidebar/>
-            <section className="home-section">
-                <div className="card w-75 datataker row">
-                    <h4>Straddle</h4>
-                    <form action="" className="form-group p-3 col-md-12" >
-                        
-                        <select>
-                            <option value="nifty">Nifty</option>
-                            <option value="bankNifty">Bank Nifty</option>
-                        </select>
-                        <br/>
-                        <br/>
-                        <lable>CE</lable>
-                        <select>
-                            <option value="37500">37,500</option>
-                            <option value="37600">37,600</option>
-                            <option value="37800">37,800</option>
-                            <option value="38000">38,000</option>
-                            <option value="38100">38,100</option>
-                            <option value="38200">38,200</option>
-                            <option value="38300">38,300</option>
-                            <option value="38400">38,400</option>
-                            <option value="38500">38,500</option>
-                        </select>
-                        <br/>
-                        <br/>
-                        <lable>PE</lable>
-                        <select>
-                            <option value="37500">37,500</option>
-                            <option value="37600">37,600</option>
-                            <option value="37800">37,800</option>
-                            <option value="38000">38,000</option>
-                            <option value="38100">38,100</option>
-                            <option value="38200">38,200</option>
-                            <option value="38300">38,300</option>
-                            <option value="38400">38,400</option>
-                            <option value="38500">38,500</option>
-                        </select>
-                        <br/>
-                        <br/>
-                        <lable>Date</lable>
-                        <input type="date" name="date" id="date" />
-                        <br/>
-                        <br/>
-                        <input type="submit" className="btn btn-sm btn-info" />
-                    </form>
-                </div>
-                <div className="col-md-12">
-                    <Chart
-                        options={this.state.options}
-                        series={this.state.series}
-                        type="line"
-                        width="95%"
-                        height="350px"
-                    />
-                </div>
-            </section>
+
+  return (
+    <div className="straddle">
+      <Sidebar />
+      <section className="home-section">
+        <div className="card w-75 datataker row">
+          <h4>Straddle</h4>
+          <form action="" className="form-group p-3 col-md-12" onSubmit={(e) => { handleSubmit(e); }} >
+
+            <select onChange={(e) => { setInstrument(e.target.value) }}>
+              <option value="NIFTY">NIFTY</option>
+              <option value="BANKNIFTY">BANK NIFTY</option>
+            </select>
+            <br />
+            <br />
+            <lable>CE</lable>
+            <select onChange={(e) => { setStrike(e.target.value) }}>
+              <option value={37500}>37,500</option>
+              <option value={37600}>37,600</option>
+              <option value={37800}>37,800</option>
+              <option value={38000}>38,000</option>
+              <option value={38100}>38,100</option>
+              <option value={38200}>38,200</option>
+              <option value={38300}>38,300</option>
+              <option value={38400}>38,400</option>
+              <option value={38500}>38,500</option>
+            </select>
+            <br />
+            <br />
+            <lable>PE</lable>
+            <select onChange={(e) => { setStrike(e.target.value) }}>
+              <option value={37500}>37,500</option>
+              <option value={37600}>37,600</option>
+              <option value={37800}>37,800</option>
+              <option value={38000}>38,000</option>
+              <option value={38100}>38,100</option>
+              <option value={38200}>38,200</option>
+              <option value={38300}>38,300</option>
+              <option value={38400}>38,400</option>
+              <option value={38500}>38,500</option>
+            </select>
+            <br />
+            <br />
+            <label>Date</label>
+            <input type="date" name="date" id="date" onChange={(e) => { setDate(e.target.value) }} />
+            <br />
+            <br />
+            <input type="submit" className="btn btn-sm btn-info" />
+          </form>
         </div>
-        )
-    }
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="col-md-12">
+          <Chart
+            style={{ "display": shouldDisplay }}
+            options={chartOptions}
+            series={chartSeries}
+            type="line"
+            width="95%"
+            height="100%"
+            height="350px"
+          />
+        </div>
+      </section>
+    </div>
+  )
+
 }
- 
-export default Straddle;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -12,50 +12,50 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <Sidebar/>
+        <Sidebar />
         <section className="home-section">
-            <div className="row">
-              <div className="col-6 col-md-3">
-                <div className="dashboardStatsCard">
-                  <p className="text-center">1,504</p>
-                  <p className="text-center">Data</p>
-                </div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div className="dashboardStatsCard">
-                  <p className="text-center">1,504</p>
-                  <p className="text-center">Data</p>
-                </div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div className="dashboardStatsCard">
-                  <p className="text-center">1,504</p>
-                  <p className="text-center">Data</p>
-                </div>
-              </div>
-              <div className="col-6 col-md-3">
-                <div className="dashboardStatsCard">
-                  <p className="text-center">1,504</p>
-                  <p className="text-center">Data</p>
-                </div>
+          <div className="row">
+            <div className="col-6 col-md-3">
+              <div className="dashboardStatsCard">
+                <p className="text-center">1,504</p>
+                <p className="text-center">Data</p>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
-                <BarChart/>
-              </div>
-              <div className="col-md-6">
-                <CandleStickChart/>
+            <div className="col-6 col-md-3">
+              <div className="dashboardStatsCard">
+                <p className="text-center">1,504</p>
+                <p className="text-center">Data</p>
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
-                <RegularChart/>
-              </div>
-              <div className="col-md-6">
-                <PieChart/>
+            <div className="col-6 col-md-3">
+              <div className="dashboardStatsCard">
+                <p className="text-center">1,504</p>
+                <p className="text-center">Data</p>
               </div>
             </div>
+            <div className="col-6 col-md-3">
+              <div className="dashboardStatsCard">
+                <p className="text-center">1,504</p>
+                <p className="text-center">Data</p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <BarChart />
+            </div>
+            <div className="col-md-6">
+              <CandleStickChart />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <RegularChart />
+            </div>
+            <div className="col-md-6">
+              <PieChart />
+            </div>
+          </div>
         </section>
       </div>
     )
