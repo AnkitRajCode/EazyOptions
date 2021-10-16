cb6ee0ff92a472fb1b578bb0a4cc4af7302ddef1
AnkitRajCode <rajankit432@gmail.com>
2021-10-16 21:12:45 +0530

    straddle section Ui improve

--- src/Components/Straddle.js
+++ src/Components/Straddle.js
@@ -12,7 +12,6 @@ export default function Straddle() {
   const [chartOptions, setChartOptions] = useState({});
   const [chartSeries, setChartSeries] = useState([]);
 
-
   function fetchResults(e) {
     e.preventDefault();
     let X = [];
@@ -110,104 +109,119 @@ export default function Straddle() {
                 },
               }
             }
-
             var series = [
               {
                 name: "Combined Premium",
                 data: Y
               }
             ]
-
             setChartSeries(series);
-
             setChartOptions(line);
-
             setShouldDisplay("");
-
           }
         )
       }
-
     )
-
-
-
-
   }
-
   function handleSubmit(e) {
     fetchResults(e);
   }
-
 
   return (
     <div className="straddle">
       <Sidebar />
       <section className="home-section">
-        <div className="card w-75 datataker row">
-          <h4>Straddle</h4>
-          <form action="" className="form-group p-3 col-md-12" onSubmit={(e) => { handleSubmit(e); }} >
+        <div className="straddleCard">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="text-center hide-sm">STRADDLE</div>
+            </div>
+            <div className="col-md-2">
+              <div className="text-center hide-sm">CE</div>
+            </div>
+            <div className="col-md-2">
+              <div className="text-center hide-sm">PE</div>
+            </div>
+            <div className="col-md-4">
+              <div className="text-center hide-sm">DATE</div>
+            </div>
+          </div>
 
-            <select onChange={(e) => { setInstrument(e.target.value) }}>
-              <option value="NIFTY">NIFTY</option>
-              <option value="BANKNIFTY">BANK NIFTY</option>
-            </select>
-            <br />
-            <br />
-            <lable>CE</lable>
-            <select onChange={(e) => { setStrike(e.target.value) }}>
-              <option value={37500}>37,500</option>
-              <option value={37600}>37,600</option>
-              <option value={37800}>37,800</option>
-              <option value={38000}>38,000</option>
-              <option value={38100}>38,100</option>
-              <option value={38200}>38,200</option>
-              <option value={38300}>38,300</option>
-              <option value={38400}>38,400</option>
-              <option value={38500}>38,500</option>
-            </select>
-            <br />
-            <br />
-            <lable>PE</lable>
-            <select onChange={(e) => { setStrike(e.target.value) }}>
-              <option value={37500}>37,500</option>
-              <option value={37600}>37,600</option>
-              <option value={37800}>37,800</option>
-              <option value={38000}>38,000</option>
-              <option value={38100}>38,100</option>
-              <option value={38200}>38,200</option>
-              <option value={38300}>38,300</option>
-              <option value={38400}>38,400</option>
-              <option value={38500}>38,500</option>
-            </select>
-            <br />
-            <br />
-            <label>Date</label>
-            <input type="date" name="date" id="date" onChange={(e) => { setDate(e.target.value) }} />
-            <br />
-            <br />
-            <input type="submit" className="btn btn-sm btn-info" />
+          <form action="" className="form-group p-3 col-md-12" onSubmit={(e) => { handleSubmit(e); }} >
+            <div className="row">
+              <div className="col-md-4">
+                <div className="text-center mb-3 hide-lg">STRADDLE</div>
+                <select
+                onChange={(e) => { setInstrument(e.target.value) }}
+                className="form-control straddleSelect"
+                >
+                  <option value="NIFTY">NIFTY</option>
+                  <option value="BANKNIFTY">BANK NIFTY</option>
+                </select>
+              </div>
+
+              <div className="col-md-2">
+                <div className="hide-lg mt-2 mb-1">CE</div>
+                <select
+                onChange={(e) => { setStrike(e.target.value) }}
+                className="form-control straddleSelect"
+                >
+                  <option value={37500}>37,500</option>
+                  <option value={37600}>37,600</option>
+                  <option value={37800}>37,800</option>
+                  <option value={38000}>38,000</option>
+                  <option value={38100}>38,100</option>
+                  <option value={38200}>38,200</option>
+                  <option value={38300}>38,300</option>
+                  <option value={38400}>38,400</option>
+                  <option value={38500}>38,500</option>
+                </select>
+              </div>
+
+              <div className="col-md-2">
+                <div className="hide-lg  mt-2 mb-1">SE</div>
+                <select
+                onChange={(e) => { setStrike(e.target.value) }}
+                className="form-control straddleSelect"
+                >
+                  <option value={37500}>37,500</option>
+                  <option value={37600}>37,600</option>
+                  <option value={37800}>37,800</option>
+                  <option value={38000}>38,000</option>
+                  <option value={38100}>38,100</option>
+                  <option value={38200}>38,200</option>
+                  <option value={38300}>38,300</option>
+                  <option value={38400}>38,400</option>
+                  <option value={38500}>38,500</option>
+                </select>
+              </div>
+
+              <div className="col-md-4">
+                <div className="hide-lg  mt-2 mb-1">Date</div>
+                <input
+                type="date"
+                name="date"
+                id="date"
+                onChange={(e) => { setDate(e.target.value) }} 
+                className="form-control straddleSelect"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <input type="submit" className="btn btn-sm btn-info mx-auto mt-5 straddleButton" />
+            </div>
           </form>
+          
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <div className="col-md-12">
           <Chart
             style={{ "display": shouldDisplay }}
             options={chartOptions}
             series={chartSeries}
             type="line"
-            width="95%"
-            height="100%"
+            width="98%"
             height="350px"
           />
         </div>
