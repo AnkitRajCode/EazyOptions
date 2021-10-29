096322f33acb49540ac58090cd8f1593cadd7b04
AnkitRajCode <rajankit432@gmail.com>
2021-10-30 00:07:58 +0530

    lading page done

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -9,31 +9,45 @@ const Welcome = () => {
         <div className="welcome">
             <Navbar/>
             <div className="welcomeHeader">
-                <Link to="/dashboard" className="">Get Started</Link>
+                <Link to="/dashboard" className="btn btn-sm btn-warning">Dashboard</Link>
             </div>
             <div className="welcomeSection">
                 <div className="sectionOne">
                     <div className="partOne">
-                        <div className="partOneHeading">Options</div>
-                        <div className="partOneCard">Futures</div>
-                        <div className="partOneCard">Straddle</div>
-                        <div className="partOneCard">Strategy</div>
+                        <div className="partOneHeading text-dark">Options</div>
+                        <div className="partOneCard">
+                            <Link to="/straddle" className="text-decoration-none text-dark">
+                                Straddle
+                                <i class='bx bx-grid-alt'></i>
+                            </Link>
+                        </div>
+                        <div className="partOneCard">
+                            <Link to="/straddle" className="text-decoration-none text-dark">
+                                Strategy
+                                <i class='bx bx-line-chart' ></i>
+                            </Link>
+                        </div>
+                        <div className="partOneCard">
+                            <Link to="/futures" className="text-decoration-none text-dark">
+                                Futures
+                                <i class='bx bx-line-chart' ></i>
+                            </Link>
+                        </div>
                     </div>
                     <div className="partOne">
-                        <div className="partOneHeading">Options</div>
+                        <div className="partOneHeading text-dark">Strategy Analyzer</div>
                         <div className="partCombine">Futures</div>
                     </div>
                     <div className="partOne">
                         <div className="partOneHeading">Options</div>
                         <div className="partOneCard">Futures</div>
-                        <div className="partOneCard">Straddle</div>
-                        <div className="partOneCard">Strategy</div>
+                        <div className="parttwocard"></div>
                     </div>
                 </div>
                 <div className="sectionTwo">
-                    <div className="sidepartone"></div>
-                    <div className="sideparttwo"></div>
-                    <div className="sideparttwo"></div>
+                    <div className="sidepartone shadow"></div>
+                    <div className="sideparttwo shadow"></div>
+                    <div className="sideparttwo shadow"></div>
                 </div>
             </div>
         </div>
